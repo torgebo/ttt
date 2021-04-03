bf2502f4acf129635bcad8bce92b86c2e942c2a6
Torgeir Børresen <torgebo@fastmail.fm>
2021-04-03 22:15:05 +0200

    Start empty game file with some comments and has_won function.

---
+++ ttt.py
@@ -0,0 +1,17 @@
+"""We represent a Tic Tac Toe board as a two dimensional
+3x3 list, each cell containing one of ' ', 'X', 'O'.
+"""
+
+
+def has_won(board):
+    """Given board, return 
+    - ' ' if no player has yet won
+    - 'X' if player 'X' has won
+    - 'O' if player 'O' has won.
+
+    Note
+    ----
+    We assume board has at most one winning player.
+    """
+    return ' '
+
