97a39cc2599cac43dcdfd5f2453eadb9c191c221
Torgeir Børresen <torgebo@fastmail.fm>
2021-04-03 23:05:54 +0200

    Implemented diagonal checks in has_won_player.

--- test_ttt.py
+++ test_ttt.py
@@ -2,10 +2,33 @@ import unittest
 
 from ttt import (
     has_won,
+    print_board,
 )
+
+board1 = [
+    ['X', 'X', 'X'],
+    ['O', ' ', 'O'],
+    [' ', 'O', ' '],
+]
+
+
+board2 = [
+    ['X', ' ', ' '],
+    ['O', 'X', 'O'],
+    [' ', 'O', 'X'],
+]
+
 
 
 class TestTicTacToe(unittest.TestCase):
 
-    def test_wins_X(self):
-        pass
+    def test_wins_board_1(self):
+        print_board(board1)
+        winner = has_won(board1)
+        print("winner", winner)
+
+    def test_wins_board_2(self):
+        print_board(board2)
+        winner = has_won(board2)
+        print("winner", winner)
+        self.assertEqual(winner, 'X')
--- ttt.py
+++ ttt.py
@@ -5,7 +5,7 @@
 def print_board(board):
     print(*board, sep='\n')
 
-
+    
 def has_won(board):
     """Given board, return 
     - ' ' if no player has yet won
@@ -14,7 +14,34 @@ def has_won(board):
 
     Note
     ----
+
     We assume board has at most one winning player.
     """
+    win_X = has_won_player(board, player='X')
+    if win_X:
+        return 'X'
+
+    win_O = has_won_player(board, player='O')
+    if win_O:
+        return 'O'
+
     return ' '
 
+
+def has_won_player(board, player):
+    """`player` is given as 'X' or 'O'
+
+    Returns True if there is a winning position
+    for `player`, otherwise False.
+    """
+    diag1 = True
+    diag2 = True
+    for i in range(len(board)):
+        if board[i][i] != player:
+            diag1 = False
+        if board[i][2-i] != player:
+            diag2 = False
+    if diag1 or diag2:
+        return True
+            
+    return False
